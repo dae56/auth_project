97209c79279ed928d3f96074622a3a75a357fa88
ayamushev <ayamushev@rvision.ru>
2025-09-27 00:17:15 +0400

    refactor - Update sessionmaker

--- src/connectors/db.py
+++ src/connectors/db.py
@@ -5,17 +5,18 @@ from sqlalchemy.orm import sessionmaker, Session
 
 
 def get_session() -> Session:
-    engine = create_engine(
-        url=URL.create(
-            drivername='postgresql+psycopg2',
-            host=os.getenv('DB_HOST'),
-            port=os.getenv('DB_PORT'),
-            username=os.getenv('DB_USER'),
-            password=os.getenv('DB_PASS'),
-            database=os.getenv('DB_NAME')
-        ),
-        echo=os.getenv('DB_ECHO')
+    session_factory = sessionmaker(
+        bind=create_engine(
+            url=URL.create(
+                drivername='postgresql+psycopg2',
+                host=os.getenv('DB_HOST'),
+                port=os.getenv('DB_PORT'),
+                username=os.getenv('DB_USER'),
+                password=os.getenv('DB_PASS'),
+                database=os.getenv('DB_NAME')
+            ),
+            echo=os.getenv('DB_ECHO')
+        )
     )
-    session_factory = sessionmaker(bind=engine)
     with session_factory() as session:
         yield session
