db4c8b54b6baa31d599de54af08408e726e27ee7
ayamushev <ayamushev@rvision.ru>
2025-09-27 01:44:17 +0400

    feat - Add health-check request

--- src/routers/auth.py
+++ src/routers/auth.py
@@ -1,5 +1,6 @@
+import os
 from typing import Any
-from fastapi import APIRouter, status
+from fastapi import APIRouter, Request, status
 from sqlalchemy import text
 
 from src.connectors.db import get_session
@@ -11,9 +12,13 @@ router = APIRouter(
 )
 
 
-@router.get("/healph-check")
-def root() -> dict[str, dict[str, Any]]:
-    return {
-        "app": {"status": status.HTTP_200_OK},
-        "db": {"status": 200 if get_session().scalar(text("SELECT 1")) else 500},
-    }
+@router.get("/health-check")
+def health_check(request: Request) -> dict[str, int] | int:
+    if request.headers.get("api-gateway-request") == "True":
+        if request.headers.get("api-gateway-token") == os.getenv("API_GATEWAY_TOKEN"):
+            return {
+                "app": status.HTTP_200_OK,
+                "db": 200 if get_session().scalar(text("SELECT 1")) else 500
+            }
+        return status.HTTP_403_FORBIDDEN
+    return status.HTTP_404_NOT_FOUND
